31303601fb297feb8222493215cb166b5b2d9e13
rafatieppo <rafaeltieppo@yahoo.com.br>
2021-09-10 09:23:27 -0400

    fix path .credentials

--- api_googlecal/api_cal_refresh.py
+++ api_googlecal/api_cal_refresh.py
@@ -32,7 +32,7 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('/home/rafatieppo/.credentials/'):
+    if os.path.exists('/home/rafatieppo/.credentials/token.json'):
         creds = Credentials.from_authorized_user_file(
             '/home/rafatieppo/.credentials/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
